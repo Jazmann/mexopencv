1750cf45a3015ad02d84e2bd5b5afedd5cbc9371
Jasper <jaspershemilt@virginmedia.co.uk>
2014-04-01 17:52:57 +0100

    cvtColorRot

--- src/+cv/cvtColorRot.cpp
+++ src/+cv/cvtColorRot.cpp
@@ -1,8 +1,8 @@
 /**
- * @file cvtColor.cpp
+ * @file cvtColorRot.cpp
  * @brief mex interface for cvtColor
- * @author Kota Yamaguchi
- * @date 2012
+ * @author Jasper
+ * @date 2014
  */
 #include "mexopencv.hpp"
 using namespace std;
@@ -18,8 +18,9 @@ using namespace cv;
 void mexFunction( int nlhs, mxArray *plhs[],
                   int nrhs, const mxArray *prhs[] )
 {
+    // cvtColorRot(dst,src,theta,g,c)
     // Check the number of arguments
-    if (nrhs<5 ||  nlhs>1)
+    if (nrhs<4 ||  nlhs>1)
         mexErrMsgIdAndTxt("mexopencv:error","Wrong number of arguments");
     
     // Argument vector
@@ -28,25 +29,29 @@ void mexFunction( int nlhs, mxArray *plhs[],
     // Option processing
     Mat src(rhs[0].toMat()), dst;
     const double Theta = rhs[1].toDouble();
-    std::vector<double> vg = rhs[2].toVector<double>(); 
-    std::vector<double> vc = rhs[3].toVector<double>();
-  //  double g[] = {vg[0],vg[1],vg[2]};
-  //  double c[] = {vc[0],vc[1],vc[2]};
+    std::vector<double> g_std = rhs[2].toVector<double>(); 
+    std::vector<double> c_std = rhs[3].toVector<double>();
+    double g[] = {g_std[0],g_std[1],g_std[2]};
+    double c[] = {c_std[0],c_std[1],c_std[2]};
+    Vec<double, 3> g_cv(c_std[0],c_std[1],c_std[2]);
+    Vec<double, 3> c_cv(g_std[0],g_std[1],g_std[2]);
     
+    RGB2Rot<CV_8UC3,CV_8UC3> colSpace(2, 2, Theta, g_cv, c_cv);
     
-    Vec<double, 3> c(0.5, 125.0/255.0, 107.0/255.0);
-    // cv::Vec<typename cv::depthConverter<CV_8UC4, CV_8UC3>::srcType, 3> c(180, 50, 128);
-    Vec<double, 3> g(1, 18, 2.8);
-    RGB2Rot<CV_8UC4,CV_8UC3> colSpace(2, 2, -0.1537, g, c);
-
+ //   RGB2Rot<CV_8UC3,CV_8UC3> colSpace_std(2, 2, Theta, g_std, c_std);
+    
+ //   RGB2Rot<CV_8UC3,CV_8UC3> colSpace_null;
+//      colSpace_null.setTransformFromAngle(Theta);
+//      colSpace_null.setRGBIndices(2, 2);
+//      colSpace_null.setuC(c_cv); // asumes that C is in rotated color space and with a dstBlueIdx
+//      colSpace_null.setG(g_cv);
+//      colSpace_null.setRedDistributionErf();
+//      colSpace_null.setGreenDistributionErf();
+//      colSpace_null.setBlueDistributionErf();
     // Process
-   // double t = erf(Theta);
+    double t = cv::erf(Theta);
+    double it = cv::erfinv(Theta);
     
- // "void cv::convertColor<50, 34>(cv::_InputArray const&, cv::_OutputArray const&, cv::colorSpaceConverter<50, 34>&)"
- // "cv::RGB2Rot<50, 34>::RGB2Rot(int, int, double, std::__1::vector<double, std::__1::allocator<double> >, std::__1::vector<double, std::__1::allocator<double> >)"
-
-  //  RGB2Rot<CV_8UC3,CV_8UC3> colSpace();
-  //  RGB2Rot<CV_8UC3,CV_8UC3> colSpace(2, 2, 0.125, g, c);
-  //  convertColor<CV_8UC3,CV_8UC3>(src, dst, colSpace);
+    convertColor<CV_8UC3,CV_8UC3>(src, dst, colSpace);
     plhs[0] = MxArray(dst);
 }
